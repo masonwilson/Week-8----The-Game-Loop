49294a57c3e4a0a0ea591d4f2dcc782a640b8023
masonwilson <mew8wjn@virginia.edu>
2022-10-14 15:25:58 -0400

    part of LU 4
    doesn't go infinitely but does once back and forth ????

--- Assets/LearningUnits/Unit04/Scripts/MoveBackAndForth.cs
+++ Assets/LearningUnits/Unit04/Scripts/MoveBackAndForth.cs
@@ -40,11 +40,12 @@ public class MoveBackAndForth : MonoBehaviour
 
         // NOTE: Uncomment the four code lines below, and change what is inside the "if statement" to make this work
         // You should NOT write any new lines of code-- just change what is inside the "if statement"
-
-        //if (Something...)
-        //{
-        //    moveSpeed = -moveSpeed;
-        //}
+        
+        if (moveAmount >= 1)
+        {
+            moveSpeed = -moveSpeed;
+            Debug.Log(moveAmount + " " + moveSpeed);
+        }
 
 
     }
